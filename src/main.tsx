9558b8f06bbd651b108e4a948bcae2548d87b30b
surajsharma1 <surajsharma424255@gmail.com>
2026-01-31 18:36:50 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,27 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './components/App';
-import Login from './components/Login';
-import Register from './components/Register';
 import Dashboard from './components/Dashboard';
 import TournamentView from './components/TournamentView';
 import TeamManagement from './components/TeamManagement';
 import BracketView from './components/BracketView';
 import OverlayEditor from './components/OverlayEditor';
 import './index.css';
-import ForgotPassword from './components/ForgotPassword';
-import ResetPassword from './components/ResetPassword';
-import TestPage from './components/TestPage';
 
 const router = createBrowserRouter([
-  {
-    path: '/login',
-    element: <Login />,
-  },
-  {
-    path: '/register',
-    element: <Register />,
-  },
   {
     path: '/',
     element: <App />,
@@ -33,19 +20,6 @@ const router = createBrowserRouter([
       { path: 'brackets', element: <BracketView /> },
       { path: 'overlay', element: <OverlayEditor /> },
     ],
-  },
-
-  {
-    path: '/forgot-password',
-    element: <ForgotPassword />,
-  },
-  {
-    path: '/reset-password/:token',
-    element: <ResetPassword />,
-  },
-  {
-    path: '/test',
-    element: <TestPage />,
   },
 ]);
 
