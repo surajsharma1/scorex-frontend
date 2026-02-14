7f78adf73be50e62dd4aec8757baaece9bf05ad6
surajsharma1 <surajsharma424255@gmail.com>
2026-02-15 02:01:20 +0530

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -82,12 +82,13 @@ export default function Login() {
           <button
             type="submit"
             disabled={loading}
-            className="btn-6"
+            className="btn-6 w-full"
           >
             {loading ? (
               <div className="flex items-center justify-center">
                 <div className="animate-spin rounded-full h-5 w-5 border-b-2 border-white mr-2"></div>
                 Signing In...
+
               </div>
             ) : (
               'Sign In'
@@ -108,7 +109,7 @@ export default function Login() {
           <button
             type="button"
             onClick={handleGoogleLogin}
-            className="btn-6"
+            className="btn-6 w-full"
           >
             <svg className="w-5 h-5 mr-3" viewBox="0 0 24 24">
               <path fill="currentColor" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/>
