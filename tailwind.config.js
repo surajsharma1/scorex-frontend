8d835db23d388ce71fa621730977b60e4b0f366d
surajsharma1 <surajsharma424255@gmail.com>
2026-02-15 00:14:20 +0530

    cg

--- tailwind.config.js
+++ tailwind.config.js
@@ -64,7 +64,7 @@ export default {
         warning: '#f39c12',
       },
       fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
+        sans: ['Bierika', 'Inter', 'system-ui', 'sans-serif'],
       },
     },
   },
