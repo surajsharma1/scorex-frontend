e039ea0fbd7924d478ffbb891774834354d6954a
surajsharma1 <surajsharma424255@gmail.com>
2026-03-14 18:51:32 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -66,7 +66,8 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
-        secure: false
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   },
